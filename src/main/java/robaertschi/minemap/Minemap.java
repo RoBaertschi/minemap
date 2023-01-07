d41e7f5ef8f70871076306eaa92f87c8efa359d0
RoBaertschi <rtmbaertschi007@gmail.com>
2023-01-07 12:53:37 +0100

    Initial commit

---
+++ src/main/java/robaertschi/minemap/Minemap.java
@@ -0,0 +1,10 @@
+package robaertschi.minemap;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minemap implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
